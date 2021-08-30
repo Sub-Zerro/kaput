13e52c1ea6d29201b32c923edeae6a8d89c55489
Sub-Zerro <85824405+Sub-Zerro@users.noreply.github.com>
2021-08-30 21:28:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let i = 0;
+
+while (i!=1) {
+    console.log("kfddfmdvevdmvmervkervkmkevmkemvkervqjfc ji hfuqhfhioghoh3ohquig hoe ghohqghegh heoghie ghgiheghiggigehghghghghghghghghghghghghghghghhghggijgrigirgijgirjigjrijifrjifjifjirjfijfirjfijrifjirfjirjfijfirjfirjfijfijfirjfirjfirjfirjfijrifififfjrfjfjjfjfjfjfjfjfjfjfjfjelldldldldldldlldldldldldlldldllldldlldldlldld");
+};
+
